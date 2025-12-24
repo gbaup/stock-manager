bd260be6df93a1922ab1bd5b48a78d43c616d65d
gastonbauer <gastonbauerp@gmail.com>
2025-12-24 01:51:18 -0300

    fix: migrations again

--- prisma/migrations/20251224044946_tables_creation/migration.sql
+++ prisma/migrations/20251224044946_tables_creation/migration.sql
@@ -1,33 +1,36 @@
 -- CreateTable
 CREATE TABLE "users" (
-    "id" SERIAL NOT NULL,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "name" VARCHAR(100) NOT NULL,
     "last_name" VARCHAR(100) NOT NULL,
     "email" VARCHAR(100) NOT NULL,
     "password_hash" VARCHAR(255) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "batches" (
-    "id" SERIAL NOT NULL,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "purchase_date" DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "arrival_date" DATE,
-    "total_price" DECIMAL(10,2) NOT NULL DEFAULT 0.00,
     "quantity" INTEGER NOT NULL,
     "tracking_number" VARCHAR(100),
     "description" TEXT,
     "shipping_price_usd" DECIMAL(10,2),
     "shipping_price_uyu" DECIMAL(10,2),
     "weight" DECIMAL(10,2),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "batches_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "catalog_products" (
-    "id" SERIAL NOT NULL,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "team" VARCHAR(100) NOT NULL,
     "season" VARCHAR(20) NOT NULL,
     "version" VARCHAR(50),
@@ -35,33 +38,40 @@ CREATE TABLE "catalog_products" (
     "number" INTEGER,
     "player" VARCHAR(100),
     "description" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "catalog_products_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "inventory_items" (
-    "id" SERIAL NOT NULL,
-    "catalog_product_id" INTEGER NOT NULL,
-    "batch_id" INTEGER NOT NULL,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "catalog_product_id" UUID NOT NULL,
+    "batch_id" UUID NOT NULL,
     "size" VARCHAR(10) NOT NULL,
     "base_price_usd" DECIMAL(10,2) NOT NULL,
+    "base_price_uyu" DECIMAL(10,2) NOT NULL,
     "final_price_usd" DECIMAL(10,2),
     "final_price_uyu" DECIMAL(10,2),
     "status" VARCHAR(20) NOT NULL DEFAULT 'available',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "inventory_items_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "sales" (
-    "id" SERIAL NOT NULL,
-    "inventory_item_id" INTEGER NOT NULL,
-    "user_id" INTEGER NOT NULL,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "inventory_item_id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
     "price" DECIMAL(10,2) NOT NULL,
     "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "method" VARCHAR(150),
     "description" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "sales_pkey" PRIMARY KEY ("id")
 );
